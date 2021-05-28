30979d544b8de34142fd453851e717c3d5e24059
Ando ANDRIANAVALONA <rakotofiringa.ando.andrianavalona@esti.mg>
2021-05-28 23:10:44 +0300

    update heroku token

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ bot = Bot(ACCESS_TOKEN)
 pageId = "109923754135421"
 RepoName = "YAndoRak/botmessenger"
 appName = "botmessengerjao"
-herokutoken = '64165d5c-8744-45a7-8f59-b541d2aab34a'
+herokutoken = 'fb6b5077-c164-4aa3-bc43-217e7a5b577a'
 
 ##########################
 
