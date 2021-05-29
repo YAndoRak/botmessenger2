a323f133eb2bf7ef72031ed4c78ae3e621fd4a5f
Ando ANDRIANAVALONA <rakotofiringa.ando.andrianavalona@esti.mg>
2021-05-29 22:39:19 +0300

    get json

--- app.py
+++ app.py
@@ -363,7 +363,7 @@ def send_message(recipient_id, response):
 
 def use_voucher(tel, recipient_id):
 	response = requests.get("https://botuserauth.herokuapp.com/api/subscribe/"+tel+"/"+recipient_id)
-	return response.json()
+	return response.get_json()
 
 def message_video(ytbId, recipient_id):
 	requests.get("https://nodemess2.herokuapp.com/"+ytbId+"/"+recipient_id)
