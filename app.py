6f70174782391cf31a7ce2a4a928e7dccbe54810
Ando ANDRIANAVALONA <rakotofiringa.ando.andrianavalona@esti.mg>
2021-06-01 18:30:43 +0300

    rectif

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def receive_message():
 						elif receive_postback[0] == "viewaudio":
 							forfaitleft = requests.get("https://botuserauth.herokuapp.com/api/forfait/"+recipient_id)
 							forfait_left = forfaitleft.json()
-							if(forfait_left.isdigit()):
+							if(Number.isInteger(forfait_left)):
 								if(forfait_left > 0):
 									response_query = ' '.join(map(str, receive_postback[1:]))
 									type_query = 'audio'
@@ -244,7 +244,7 @@ def receive_message():
 							print('Forfait restant', forfait_left)
 							if len(receive_postback) < 2:
 								send_message(recipient_id, 'Erreur veuillez recommencer')
-							if (forfait_left.isdigit()):
+							if (Number.isInteger(forfait_left)):
 								if(forfait_left > 0):
 									response_query = ' '.join(map(str, receive_postback[1:]))
 									send_generic_template_download_youtube(recipient_id, response_query)
